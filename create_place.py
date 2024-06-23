e77161d055263570aec99dad674977161a385a6c
Moonllee <polubomy.max@gmail.com>
2024-06-23 13:04:56 +0300

    2.1

--- create_place.py
+++ create_place.py
@@ -12,14 +12,3 @@ def create_place(data):
     response_data = response.json()
     return response_data
 
-############
-
-data_1 = {
-    'title': 'Test_1',
-    'lat': 55.028254,
-    'lon': 82.918501,
-    'color': 'BLUE'
-}
-
-if __name__ == '__main__':
-    create_place(data_1)
